7554ab5fc8cff07aab389690d92b7ff0d45bb45a
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-13 16:46:48 +0530

    one

---
+++ one.java
@@ -0,0 +1,9 @@
+public class one {
+    public static void main(String[] args) {
+        String str = "hello";
+        for(int i = str.length()-1; i >= 0 ; i--){
+            System.out.print(str.charAt(i));
+        }
+        System.out.println();
+    }
+}
