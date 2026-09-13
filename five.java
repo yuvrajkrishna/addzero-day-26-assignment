033044a7a447093d16f9d3d1de50122b63931618
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-13 17:43:24 +0530

    five

--- five.java
+++ five.java
@@ -1,7 +1,13 @@
 public class five {
     public static void main(String[] args) {
         String str = "hello";
-        String st2 = "hello";
-        System.out.println(str==st2);
+        String st2 = new String("hello");
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != st2.charAt(i)){
+                System.out.println("Not Same");
+                return;
+            }
+        }
+        System.out.println("Same");
     }
 }
