301c18a0e0ab57e7b5743368f3d89ee62105ed77
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-13 17:35:55 +0530

    four

--- four.java
+++ four.java
@@ -2,16 +2,9 @@ public class four {
     public static void main(String[] args) {
         String str = "Hello   How   are ";
 
-        int count = 0;
+        String arr[] = str.trim().split("\\s+");
 
-        for (int i = 0; i < str.length(); i++) {
-
-            if (str.charAt(i) != ' ' &&
-                    (i == 0 || str.charAt(i - 1) == ' ')) {
-                count++;
-            }
-        }
-
-        System.out.println(count);
+        System.out.println(arr.length);     
     }
 }
+
