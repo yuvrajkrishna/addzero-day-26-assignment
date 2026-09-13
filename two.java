f6411278d2075c00122e619392eef043ef1d9f2f
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-09-13 16:56:03 +0530

    two

--- two.java
+++ two.java
@@ -1,11 +1,16 @@
 public class two {
     public static void main(String[] args) {
         String str = "hello";
-        String rev = "";
-        
-        for(int i = str.length()-1; i >= 0 ; i--){
-            rev += str.charAt(i);
+        char ch[] = str.toCharArray();
+        int left = 0 ;
+        int right = str.length()-1;
+        while(left < right){
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+            left++;
+            right--;
         }
-        System.out.println(rev);
+        System.out.println(ch);
     }
 }
